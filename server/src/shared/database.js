ab7c3c86881c511362e89c3d8e4acb78991d85c9
Anudeep <ankrishn@teksystems.com>
2018-11-25 15:19:22 +0530

    added proc file

--- server/src/shared/database.js
+++ server/src/shared/database.js
@@ -3,8 +3,9 @@ const config = require('./config/config');
 
 Mongoose.Promise = global.Promise;
 //Mongoose.connect(config.mongodb.host);
+//heroku config:set MONGOLAB_URI= mongodb://ankrishn:Thisis@mongo1@tcp-shard-00-00-dtnhv.mongodb.net:27017,tcp-shard-00-01-dtnhv.mongodb.net:27017,tcp-shard-00-02-dtnhv.mongodb.net:27017/test?ssl=true&replicaSet=TCP-shard-0&authSource=admin
 Mongoose.connect('mongodb://tcp-shard-00-00-dtnhv.mongodb.net:27017,tcp-shard-00-01-dtnhv.mongodb.net:27017,tcp-shard-00-02-dtnhv.mongodb.net:27017/test?ssl=true&replicaSet=TCP-shard-0&authSource=admin',
-                 {user: 'ankrishn', pass: 'Thisis@mongo1'},
+                 {user: 'ankrishn', pass: 'welcome!23'},
                  function callback() {
 					console.log("Connection with database succeeded.");
 				});
